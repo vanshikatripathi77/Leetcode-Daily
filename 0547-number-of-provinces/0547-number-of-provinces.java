d29073eaadcd7ced2149007dbefd814e5688a9e5
vanshikatripathi77 <134995435+vanshikatripathi77@users.noreply.github.com>
2026-04-22 09:52:15 +0530

    Time: 5 ms (10.35%), Space: 50 MB (6.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,29 +3,30 @@ class Solution {
         List<List<Integer>> adj=new ArrayList<>();
         int n=isConnected.length;
         for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<>());
         }
         for(int i=0;i<isConnected.length;i++){
             for(int j=0;j<isConnected[i].length;j++){
-                if(isConnected[i][j]==1 && i!=j ){
+                if(isConnected[i][j]==1 && i!=j){
                     adj.get(i).add(j);
                 }
             }
         }
         Queue<Integer> q=new LinkedList<>();
+        boolean[] vis=new boolean[n];
         int ct=0;
-        boolean[] vis=new boolean[n+1];
         for(int i=0;i<n;i++){
-            ct++;
-            vis[i]=true;
-            q.add(i);
-            while(!q.isEmpty()){
-                int x=q.poll();
-                vis[x]=true;
-                for(int j:adj.get(x)){
-                    if(!vis[j]){
-                        vis[j]=true;
-                        q.add(j);
+            if(!vis[i]){
+                ct++;
+                q.add(i);
+                while(!q.isEmpty()){
+                    int curr=q.poll();
+                    vis[i]=true;
+                    for(int x:adj.get(curr)){
+                        if(!vis[x]){
+                            vis[x]=true;
+                            q.add(x);
+                        }
                     }
                 }
             }
