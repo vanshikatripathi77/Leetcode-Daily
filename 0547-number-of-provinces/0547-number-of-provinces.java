6caf97311489e668460c18a9745e79e264c92921
vanshikatripathi77 <134995435+vanshikatripathi77@users.noreply.github.com>
2026-05-19 00:38:23 +0530

    Time: 3 ms (27.56%), Space: 49.7 MB (7.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,39 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adj=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         int n=isConnected.length;
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected[i].length;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1 && i!=j){
                     adj.get(i).add(j);
                 }
             }
         }
-        Queue<Integer> q=new LinkedList<>();
-        boolean[] vis=new boolean[n];
         int ct=0;
+        boolean[] visited=new boolean[n];
         for(int i=0;i<n;i++){
-            if(!vis[i]){
+            if(!visited[i]){
                 ct++;
-                q.add(i);
-                while(!q.isEmpty()){
-                    int curr=q.poll();
-                    vis[i]=true;
-                    for(int x:adj.get(curr)){
-                        if(!vis[x]){
-                            vis[x]=true;
-                            q.add(x);
-                        }
-                    }
+                bfs(i,adj,visited);
+            }
+        }
+        return ct++;
+    }
+    public void bfs(int i,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            visited[curr]=true;
+            for(int x:adj.get(curr)){
+                if(!visited[x]){
+                    q.add(x);
+                    visited[x]=true;
                 }
             }
         }
-        return ct;
     }
 }
