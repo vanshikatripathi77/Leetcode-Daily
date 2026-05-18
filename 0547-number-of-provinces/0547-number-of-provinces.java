9841c857c3c46f2532d88294c49a9779f0956dc7
vanshikatripathi77 <134995435+vanshikatripathi77@users.noreply.github.com>
2026-05-19 00:55:06 +0530

    Time: 2 ms (43.21%), Space: 50 MB (5.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         int n=isConnected.length;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
-        }
+        } 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1 && i!=j){
@@ -17,22 +17,16 @@ class Solution {
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 ct++;
-                bfs(i,adj,visited);
+                dfs(i,adj,visited);
             }
-        }
-        return ct++;
+        } 
+        return ct;    
     }
-    public void bfs(int i,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
-        Queue<Integer> q=new LinkedList<>();
-        q.add(i);
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            visited[curr]=true;
-            for(int x:adj.get(curr)){
-                if(!visited[x]){
-                    q.add(x);
-                    visited[x]=true;
-                }
+    public void dfs(int i,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
+        visited[i]=true;
+        for(int x:adj.get(i)){
+            if(!visited[x]){
+                dfs(x,adj,visited);
             }
         }
     }
