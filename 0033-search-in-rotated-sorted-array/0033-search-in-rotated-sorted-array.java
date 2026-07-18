4dfcba99e911b43cb91d5a2b76d7b8cc4bcfcd41
vanshikatripathi77 <134995435+vanshikatripathi77@users.noreply.github.com>
2026-07-18 16:21:08 +0530

    Time: 1 ms (2.11%), Space: 44.7 MB (11.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -11,25 +11,27 @@ class Solution {
                 break;
             }
         }
-        int[] arr=new int[nums.length];
-        int c=0;
-        for(int i=index;i<arr.length;i++){
-            arr[c++]=nums[i];
-        }
-        for(int i=0;i<index;i++){
-            arr[c++]=nums[i];
-        }
-        for(int i:arr){
-            System.out.println(i);
-        }
-        int left=0,right=nums.length-1;
+        int left=0,right=index-1;
         int mid=0;
         while(left<=right){
             mid=(left+right)/2;
-            if(arr[mid]<target){
+            if(nums[mid]<target){
                 left=mid+1;
             }
-            else if(arr[mid]>target){
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                return hp.get(target);
+            }
+        }  
+        left=index;right=nums.length-1;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            else if(nums[mid]>target){
                 right=mid-1;
             }
             else{
